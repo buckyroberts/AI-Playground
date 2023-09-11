1f99677c497819ea10b2c82091019d1b2f3458b1
bucky <buckyroberts@gmail.com>
2023-09-11 12:51:12 -0400

    Large text summary

---
+++ bots/13_large_text_summary/main.py
@@ -0,0 +1,44 @@
+import os
+import openai
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+def generate_summary(text):
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=f'Summarize the following:\n{text}',
+        max_tokens=150,
+    )
+    return response.choices[0].text.strip()
+
+
+def split_text_into_chunks(text, chunk_size=5000):
+    chunks = []
+
+    for i in range(0, len(text), chunk_size):
+        chunk = text[i:i + chunk_size]
+        chunks.append(chunk)
+
+    return chunks
+
+
+def main():
+    with open('./woodpecker.txt', 'r') as file:
+        content = file.read()
+
+    # Split the content into chunks
+    chunks = split_text_into_chunks(content)
+
+    # Generate summaries for each chunk
+    chunk_summaries = [generate_summary(chunk) for chunk in chunks]
+
+    # Combine chunk summaries and generate a final summary
+    combined_text = ' '.join(chunk_summaries)
+    final_summary = generate_summary(combined_text)
+
+    print('Final Summary:\n', final_summary)
+
+
+if __name__ == '__main__':
+    main()
