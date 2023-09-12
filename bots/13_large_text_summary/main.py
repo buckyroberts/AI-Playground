3bde0783fb5e8f7846decd839561ca61bc434102
bucky <buckyroberts@gmail.com>
2023-09-12 13:51:18 -0400

    Unify large text summarizers

--- bots/13_large_text_summary/main.py
+++ bots/13_large_text_summary/main.py
@@ -1,5 +1,7 @@
 import os
+
 import openai
+from PyPDF2 import PdfReader
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
@@ -15,8 +17,34 @@ def generate_summary(text):
     )
     summary = response.choices[0].message['content'].strip()
     print(summary)
-    print('*' * 120)
+    print('-' * 120)
     return summary
+
+
+def pdf_to_text(pdf_path):
+    reader = PdfReader(pdf_path)
+    extracted_texts = []
+
+    for page in reader.pages:
+        extracted_texts.append(page.extract_text())
+
+    text = ' '.join(extracted_texts)  # join all the extracted text fragments with spaces in between
+    text = text.replace('\n', ' ')  # replace all newline characters with spaces
+
+    return text
+
+
+def summarize_large_content(content):
+    # Split the content into chunks
+    chunks = split_text_into_chunks(content)
+
+    # Generate summaries for each chunk
+    chunk_summaries = [generate_summary(chunk) for chunk in chunks]
+
+    # Combine chunk summaries and generate a final summary
+    combined_text = ' '.join(chunk_summaries)
+
+    return generate_summary(combined_text)
 
 
 def split_text_into_chunks(text, chunk_size=32_000):
@@ -29,21 +57,21 @@ def split_text_into_chunks(text, chunk_size=32_000):
     return chunks
 
 
-def main():
-    with open('./woodpecker.txt', 'r') as file:
+def summarize_txt_document(txt_path):
+    with open(txt_path, 'r') as file:
         content = file.read()
 
-    # Split the content into chunks
-    chunks = split_text_into_chunks(content)
+    summary = summarize_large_content(content)
+    print('Text Summary:\n', summary)
 
-    # Generate summaries for each chunk
-    chunk_summaries = [generate_summary(chunk) for chunk in chunks]
 
-    # Combine chunk summaries and generate a final summary
-    combined_text = ' '.join(chunk_summaries)
-    final_summary = generate_summary(combined_text)
-    print('Final Summary:\n', final_summary)
+def summarize_pdf_document(pdf_path):
+    content = pdf_to_text(pdf_path)
+    summary = summarize_large_content(content)
+    print('PDF Summary:\n', summary)
 
 
 if __name__ == '__main__':
-    main()
+    summarize_txt_document('./woodpecker.txt')
+    print('*' * 120)
+    summarize_pdf_document('./ants.pdf')
