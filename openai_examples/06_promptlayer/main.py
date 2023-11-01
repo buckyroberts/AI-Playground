7b546f903aef8a229c831f7f4a56685f4ebe8e31
Ian <iantskon@gmail.com>
2023-11-01 09:56:09 -0500

    Update 06_promptlayer code to use Chat instead of Completions

--- openai_examples/06_promptlayer/main.py
+++ openai_examples/06_promptlayer/main.py
@@ -1,20 +1,23 @@
-import os
-
 import promptlayer
+import os
+promptlayer.api_key = os.environ.get('PROMPTLAYER_API_KEY')
 
 openai = promptlayer.openai
-openai.api_key = os.getenv('OPENAI_API_KEY')
-promptlayer.api_key = os.environ.get('PROMPTLAYER_API_KEY')
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 """
 Makes your request to OpenAI and then log the request to PromptLayer
 """
 
-response = openai.Completion.create(
-    engine='text-davinci-003',
-    prompt='Recommend a book.',
+response = openai.ChatCompletion.create(
+    model='gpt-3.5-turbo',
+    messages=[
+        { 'role': 'system', 'content': 'You are a helpful assistant.' },
+        { 'role': 'user', 'content': 'Who was the first president of the United States?' }
+    ],
+    temperature=0.5,
     max_tokens=64,
-    pl_tags=['movies']
+    pl_tags=['history']
 )
 
 print(response)
