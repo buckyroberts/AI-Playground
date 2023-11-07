efc036d53d1c6b3ab02fb659e2545ae5d5a6cb0e
Ian Schoonover <6356890+nax3t@users.noreply.github.com>
2023-11-07 14:43:14 -0600

    Revisions (#2)
    * Update folder numbers in openai_examples
    
    * Update 06_promptlayer code to use Chat instead of Completions
    
    * Switch prompt_templates and scoring

---
+++ openai_examples/06_promptlayer/main.py
@@ -0,0 +1,23 @@
+import promptlayer
+import os
+promptlayer.api_key = os.environ.get('PROMPTLAYER_API_KEY')
+
+openai = promptlayer.openai
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+"""
+Makes your request to OpenAI and then log the request to PromptLayer
+"""
+
+response = openai.ChatCompletion.create(
+    model='gpt-3.5-turbo',
+    messages=[
+        { 'role': 'system', 'content': 'You are a helpful assistant.' },
+        { 'role': 'user', 'content': 'Who was the first president of the United States?' }
+    ],
+    temperature=0.5,
+    max_tokens=64,
+    pl_tags=['US Presidents']
+)
+
+print(response)
