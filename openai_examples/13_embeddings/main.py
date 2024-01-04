8af859325a6aff390c293429aa21cc5e23f1f9a6
Ian <iantskon@gmail.com>
2024-01-04 12:36:17 -0600

    Update 13_embeddings/main.py: wrap API call in a function

--- openai_examples/13_embeddings/main.py
+++ openai_examples/13_embeddings/main.py
@@ -1,33 +1,30 @@
-from openai import OpenAI
 import numpy as np
+from openai import OpenAI
+
+client = OpenAI()
 
 def cosine_similarity(a, b):
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
-client = OpenAI()
+def get_embedding(input, model):
+    return client.embeddings.create(
+        input=input,
+        model=model,
+    )
 
 MODEL = 'text-embedding-ada-002'
 
-response = client.embeddings.create(
-    input="corn",
-    model=MODEL,
-)
+response = get_embedding('corn', MODEL)
 print(response)
 print(len(response.data[0].embedding))
 
-response = client.embeddings.create(
-    input=["cat", "feline"],
-    model=MODEL,
-)
+response = get_embedding(["cat", "feline"], MODEL)
 a = response.data[0].embedding
 b = response.data[1].embedding
 score = cosine_similarity(a, b)
 print(f"Similarity score between 'cat' and 'feline': {score}")
 
-response = client.embeddings.create(
-    input=["elephant", "microscope"],
-    model=MODEL,
-)
+response = get_embedding(["elephant", "microscope"], MODEL)
 a = response.data[0].embedding
 b = response.data[1].embedding
 score = cosine_similarity(a, b)
